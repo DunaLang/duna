03fc3030e19ce303d212b379f90e0515fa7549db
Nathãn Vitor de Lima <nathan2001lima@gmail.com>
2024-06-12 10:14:24 -0300

    refactor: int to size_t in generateNames and remove not defined function

--- lib/utils.h
+++ lib/utils.h
@@ -1,10 +1,10 @@
 #pragma once
 
+#include <stddef.h>
 #include "record.h"
 
-static int counter = 0;
+static size_t counter = 0;
 
-char *cat(char *s1, char *s2, char *s3, char *s4, char *s5);
 _Bool equalTypes(record *r1, record *r2);
 _Bool isNumeric(const record *rec);
 _Bool isInteger(const record *rec);
