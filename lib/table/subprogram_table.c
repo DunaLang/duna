2c36469b0841d18fabd7c2f0a19b3f5cf2cf2166
Nathãn Vitor de Lima <nathan2001lima@gmail.com>
2024-06-19 11:22:16 -0300

    feat: tabela de subprogramas

---
+++ lib/table/subprogram_table.c
@@ -0,0 +1,59 @@
+#include "subprogram_table.h"
+#include <string.h>
+
+struct SubprogramTable createSubprogramTable(void)
+{
+    struct SubprogramTable table;
+    for (size_t i = 0; i < HASH_SIZE; i++)
+    {
+        table.nodes[i] = NULL;
+    }
+
+    return table;
+}
+
+struct SubprogramType *lookupSubprogramTable(struct SubprogramTable *this, char *key)
+{
+    size_t index = hash(key);
+    struct SubprogramTableNode *node = this->nodes[index];
+    while (node != NULL)
+    {
+        if (strcmp(key, node->key) == 0)
+        {
+            return &node->value;
+        }
+        node = node->next;
+    }
+
+    return NULL;
+}
+
+struct SubprogramTableNode *newSubprogramTableNode(char *key, struct SubprogramType type)
+{
+    struct SubprogramTableNode *node = malloc(sizeof(struct SubprogramTableNode));
+    node->key = strdup(key);
+    node->value = type;
+    node->next = NULL;
+    return node;
+}
+
+void insertSubprogramTable(struct SubprogramTable *this, char *key, struct SubprogramType type)
+{
+    size_t index = hash(key);
+    struct SubprogramTableNode *node = this->nodes[index];
+    struct SubprogramTableNode *previous = NULL;
+    while (node != NULL && strcmp(key, node->key) != 0)
+    {
+        previous = node;
+        node = node->next;
+    }
+
+    if (previous == NULL)
+    {
+        this->nodes[index] = newSubprogramTableNode(key, type);
+    }
+    else
+    {
+        previous->next = newSubprogramTableNode(key, type);
+    }
+}
---
+++ lib/table/subprogram_table.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <stdlib.h>
+#include "hashmap_utils.h"
+
+struct SubprogramTable
+{
+    struct SubprogramTableNode *nodes[HASH_SIZE];
+};
+
+struct SubprogramType
+{
+    char **parametersType;
+    size_t parametersLength;
+
+    char *returnType;
+};
+
+struct SubprogramTableNode
+{
+    char *key;
+    struct SubprogramType value;
+
+    struct SubprogramTableNode *next;
+};
+
+struct SubprogramTable createSubprogramTable(void);
+struct SubprogramType *lookupSubprogramTable(struct SubprogramTable *this, char *key);
+void insertSubprogramTable(struct SubprogramTable *this, char *key, struct SubprogramType type);
